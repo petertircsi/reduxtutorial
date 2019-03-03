8ce867ba4d948265a8b6d8a0b31624bd336e4d8a
Tircsi Peter <tircsi.peter@gmail.com>
2019-03-03 16:53:13 +0100

    Initial App Structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <h1>Header here....</h1>
+                <Header/>
                 {this.props.children}
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 render (
     <Router history={browserHistory} routes={routes} />,
     document.getElementById('app')
-)
+);
